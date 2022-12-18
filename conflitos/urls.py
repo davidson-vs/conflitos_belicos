39a1580f852cb0cdd9669a6fa6aa1a8390c380a2
Davidson Vieira de Souza <davidson.vieiras27@gmail.com>
2022-12-18 01:45:51 -0300

    ajustes nas rotas, views e inicio do get de dados dos forms

--- conflitos/urls.py
+++ conflitos/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from conflitos.views import homepage
+from conflitos.views import *
 
 
 urlpatterns = [
-    path('', homepage) 
+    path('', homepage , name= 'homepage'),
+    path('chefe_militar/', chefe_militar, name= 'chefe_militar'),
+    path('conflitos/', conflitos, name= 'conflitos'),
+    path('divisao/', divisao, name= 'divisao'),
+    path('grafics/', grafics_page, name= 'grafics'),
+    path('grupo_armado/', grupo_armado, name= 'grupo_armado'),
+    path('lideres_politicos/', liders_politicos, name= 'lideres_politicos'),
+    path('lists/', lists_page, name= 'lists'),
+    path('tipo_conflito/', tipo_conflito, name= 'tipo_conflito'),
+    path('form/', form, name= 'teste-form')
 ]
--- conflitos/views.py
+++ conflitos/views.py
@@ -1,15 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+def form(request):
+    if request.method == 'POST':
+        # aqui vc recebe o valor de cada campo do form
+        nm_divisao = request.POST.get('nome_divisao', None)
+    return render(request, 'tipo_conflito.html', status=200, context={})
 
 def homepage(request):
-    return render(request, 'pages/index.html', status=200, context={})
+    return render(request, 'index.html', status=200, context={})
 
 def grafics_page(request):
-    ...
+    return render(request, 'grafics.html', status=200, context={})
 
 def lists_page(request):
-    ...
+    return render(request, 'lists.html', status=200, context={})
 
 
+def chefe_militar(request):
+    return render(request, 'chefe_militar.html', status=200, context={})
 
+def conflitos(request):
+    return render(request, 'conflitos.html', status=200, context={})
+
+def divisao(request):
+    return render(request, 'divisao.html', status=200, context={})
+
+def grupo_armado(request):
+    return render(request, 'grupo_armado.html', status=200, context={})
+
+def liders_politicos(request):
+    return render(request, 'lideres_politicos.html', status=200, context={})
+
+def tipo_conflito(request):
+    return render(request, 'tipo_conflito.html', status=200, context={})
 
