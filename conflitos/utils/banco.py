cffce20c482c2e56c28718abbb26cad0ddb898fe
Davidson Vieira de Souza <davidson.vieiras27@gmail.com>
2022-12-18 17:11:12 -0300

    Conectando ao banco

---
+++ conflitos/utils/banco.py
@@ -0,0 +1,74 @@
+import psycopg2 as pg
+ 
+class Banco:
+    @staticmethod
+    def connection(host: str = 'localhost', database:str = 'db_conflitos_belicos', user:str ='postgres', password: str = ""):
+        try:
+            cxn = pg.connect(f'host = {host} database={database} user = {user} password = {password}')
+            if cxn is True:
+                return cxn
+        except Exception as cxn_error:
+            print(f'erro ao se conectar ao banco de dados: {cxn_error}')
+
+    @staticmethod
+    def execute_query(cxn, query, params=None, many=False, persistence=False):
+        cursor = cxn.cursor()
+        try:
+            if many is False:
+                if params is None:
+                    cursor.execute(query)
+                else:
+                    cursor.execute(query, params)
+            else:
+                cursor.fast_executemany = True
+                cursor.executemany(query, params)
+
+            if persistence:
+                cursor.commit()
+                if cursor.rowcount > 1:
+                    print('Affected rows: {}'.format(cursor.rowcount))
+        except pg.IntegrityError as ie:
+            print('An integrity error has occurred.', exc_info=True)
+            raise ie
+        except Exception as e:
+            print("Error while running a query execution: {}".format(e))
+            raise Exception("Error while running a query execution: {}".format(e))
+        finally:
+            cursor.close()
+
+    @staticmethod
+    def get_multiple_result(cxn, query, *args):
+        """
+        Método que retorna multiplos resultados do banco
+        :param cxn: pyodbc cxn obj
+        :param query: str
+        :param args: tuple
+        :return: dataset
+        """
+        cursor = cxn.cursor()
+        results = []  # Lista para armazenamento dos resultados junto os nomes de colunas
+        try:
+            cursor.execute(query, *args)
+            columns = [column[0] for column in cursor.description]  # Capturando as colunas do dataset
+            for row in cursor.fetchall():  # Para cada linha do resultado do banco, mesclar o resultado do banco junto ao nome da coluna.
+                results.append(dict(zip(columns, row)))
+            return results
+        except Exception as error:
+            raise Exception('Erro ao executar a query "{}" \n \n ERRO: {}'.format(query, error))
+        finally:
+            cursor.close()
+
+    def execute_proceudure(self, cxn, query):
+        no_count = 'SET NOCOUNT ON;'
+        proc = no_count + query
+        result = self.get_multiple_result(cxn, proc)
+        return result
+
+teste = Banco()
+
+cxn = teste.connection()
+
+if cxn:
+    print('boa')
+else: 
+    print('xiiii')
--- conflitos/views.py
+++ conflitos/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from conflitos.utils.banco import Banco
+import psycopg2
+
 
 def form(request):
-    if request.method == 'POST':
-        # aqui vc recebe o valor de cada campo do form
-        nm_divisao = request.POST.get('nome_divisao', None)
+    
     return render(request, 'tipo_conflito.html', status=200, context={})
 
 def homepage(request):
@@ -20,10 +21,32 @@ def chefe_militar(request):
     return render(request, 'chefe_militar.html', status=200, context={})
 
 def conflitos(request):
-    return render(request, 'conflitos.html', status=200, context={})
+    if request.method == 'POST':
+        # aqui vc recebe o valor de cada campo do form
+        return render(request, 'conflitos.html', status=200, context={})
 
 def divisao(request):
-    return render(request, 'divisao.html', status=200, context={})
+    try:
+        db = Banco()
+        context = {}
+        cxn = db.connection()
+        query = """ INSERT INTO """
+        if request.method == 'POST':
+            # aqui vc recebe o valor de cada campo do form
+            nm_divisao = request.POST.get('nome_divisao', None)
+            qtde_barcos = request.POST.get('qtde_barcos', None)
+            qtde_tanques = request.POST.get('qtde_tanques', None)
+            qtde_homens = request.POST.get('qtde_homens', None)
+            qtde_avioes = request.POST.get('qtde_avioes', None)
+            qtde_baixas = request.POST.get('qtde_baixas', None)
+            select_divisao = request.POST.get('select_divisao', None)
+
+            db.execute_query()
+            print(nm_divisao)
+    except Exception as tp_conflito_error:
+        print(tp_conflito_error)
+    finally:
+        return render(request, 'divisao.html', status=200, context={})
 
 def grupo_armado(request):
     return render(request, 'grupo_armado.html', status=200, context={})
@@ -32,5 +55,4 @@ def liders_politicos(request):
     return render(request, 'lideres_politicos.html', status=200, context={})
 
 def tipo_conflito(request):
-    return render(request, 'tipo_conflito.html', status=200, context={})
-
+        return render(request, 'tipo_conflito.html', status=200, context={})
